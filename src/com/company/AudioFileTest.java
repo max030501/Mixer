9f135c2e9b03aa16a59e072197333e1152d78107
max030501 <max030511@gmail.com>
2022-03-01 13:09:22 +0300

    refactor

--- src/com/company/AudioFileTest.java
+++ src/com/company/AudioFileTest.java
@@ -10,38 +10,42 @@ import java.io.IOException;
 import static org.junit.Assert.*;
 
 public class AudioFileTest {
-    private AudioFile af;
+    private AudioFile test1;
+    private AudioFile test2;
+    private AudioFile test3;
+
 
     @Before
     public void setUp() throws UnsupportedAudioFileException, IOException {
-        af = new AudioFile("test1.wav");
+        test1 = new AudioFile("test1.wav");
+        test2 = new AudioFile("test2.wav");
+        test3 = new AudioFile("test3.wav");
     }
 
     @Test
     public void timeDuration() {
-        Assert.assertEquals(af.getLength(),130);
+        Assert.assertEquals(test1.getLength(),130);
     }
 
     @Test
     public void cropAudio() throws Exception {
-        af.crop(10,15);
+        test1.crop(10,15);
         AudioFile naf = new AudioFile("test1_crop.wav");
         Assert.assertEquals(naf.getLength(),5);
     }
 
     @Test(expected = Exception.class)
     public void cropAudio_incorrectInput() throws Exception {
-        af.crop(10,10000);
+        test1.crop(10,10000);
     }
 
     @Test
     public void concatAudio() throws UnsupportedAudioFileException, IOException {
-        AudioFile naf = new AudioFile("test2.wav");
-        AudioFile naf1 = new AudioFile("test3.wav");
-        AudioFile.concat(af,naf,naf1);
+        AudioFile.concat(test1,test2,test3);
         AudioFile con = new AudioFile("test1-test2-test3.wav");
         Assert.assertEquals(con.getLength(),488);
     }
+
 
 
 }
